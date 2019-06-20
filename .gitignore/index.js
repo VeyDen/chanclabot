d53dff5c1737fde6456cdd547452c503b033e232
VeyDen <50058218+VeyDen@users.noreply.github.com>
2019-06-20 17:24:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NTkxMDc0NzYyNjQyNjg1OTcy.XQt9bg.kMdctI_9she8yEHeIdoJUwotlLY';
-
 client.on('ready', ()  =>{
     client.user.setGame("c!help", "pour voir les commandes")
     console.log('This bot is ready') ;
